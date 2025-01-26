999c9459f38dda45b09ee03812493f39fb72fd43
Hanner Totena <133254747+hatotenap@users.noreply.github.com>
2025-01-25 19:03:16 -0500

    Actualización lab 7

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -16,3 +16,91 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import os
+    # Verificar que las carpetas necesarias existan
+    os.makedirs('files/plots', exist_ok=True)
+    plt.figure()
+    # Se degrada el color gris para cada columna 
+    colors={
+        'Television':'dimgray',
+        'Newspaper': 'grey',
+        'Internet': 'tab:blue',
+        'Radio': 'Lightgrey',
+    }
+    # Contiene los nombres de las funtes y la precedencia, mientras más alto queda por encima de los demás
+    zorder = {
+        'Television': 1,
+        'Newspaper' : 1,
+        'Internet' :2,
+        'Radio' :1,
+    }
+    linewidths ={
+        'Television':2,
+        'Newspaper':2,
+        'Internet':4,
+        'Radio':2,
+    }
+    df = pd.read_csv('files/input/news.csv', index_col=0)
+    for col in df.columns:
+        plt.plot(
+            df[col],
+             color=colors[col],
+              label=col,
+              zorder= zorder[col],
+              linewidth=linewidths[col],
+              )
+    plt.title("how people get their news", fontsize=16)
+
+    #Con esto se hacen invisbles las lineas del cuadro que encierra la grafica
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    # vamos a hacer invisible a el eje y
+    plt.gca().axes.get_yaxis().set_visible(False)
+    # vamos a colocar el nombre y el valor del procentaje a la izquiera de cada línea 
+    for col in df.columns:
+        first_year=df.index[0]
+        plt.scatter(
+            x=first_year,
+            # la columna del primer punto (first_year)
+            y=df[col][first_year],
+            color= colors[col],
+            zorder= zorder[col], 
+        )
+        plt.text(
+            first_year - 0.2,
+            df[col][first_year],
+            col +" "+str(df[col][first_year])+"%",
+            # alienación horizontal derecha
+            ha='right',
+            # alineación vertical 
+            va='center',
+            color=colors[col],
+        )
+        last_year= df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y= df[col][last_year],
+            color=colors[col],
+        )
+        plt.text(
+            last_year - 0.2,
+            df[col][last_year],
+            col +" "+str(df[col][first_year])+"%",
+            # alienación horizontal derecha
+            ha='left',
+            # alineación vertical 
+            va='center',
+            color=colors[col],
+        )
+    plt.xticks(
+        ticks=df.index,
+        labels=df.index,
+        ha='center',
+    )    
+    plt.tight_layout()
+
+    plt.savefig('files/plots/news.png')
+    plt.show
